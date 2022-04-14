b51b5f052f384afb542634926c8350a7498a4d02
Abhijit070 <90260225+Abhijit070@users.noreply.github.com>
2022-04-14 16:38:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/1bhPBh0qG/model.json',modelReady)
+
+}
+
+function modelReady(){
+    classifier.classify(gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        
+    }
+}
